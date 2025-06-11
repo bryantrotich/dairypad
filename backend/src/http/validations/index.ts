40aa6ffa470efa2427be5a66bf2c35811702d487
Bryant Rotich <bryantkrotich@gmail.com>
2025-06-11 13:29:18 +0300

    Update: new file exports

--- backend/src/http/validations/index.ts
+++ backend/src/http/validations/index.ts
@@ -1,4 +1,5 @@
 export { CreateCustomerValidation as CreateCustomerValidation } from './customer/create.validation';
+export { CreateExpenseValidation as CreateExpenseValidation } from './expense/create.validation';
 export { CreateExpenseTypeValidation as CreateExpenseTypeValidation } from './expense-type/create.validation';
 export { CreateProductValidation as CreateProductValidation } from './product/create.validation';
 export { CreateSocietyValidation as CreateSocietyValidation } from './society/create.validation';
--- backend/src/modules/index.ts
+++ backend/src/modules/index.ts
@@ -1,5 +1,7 @@
 export { CompanyModule as CompanyModule } from './company.module';
 export { CustomerModule as CustomerModule } from './customer.module';
+export { ExpenseModule as ExpenseModule } from './expense.module';
+export { ExpenseTypeModule as ExpenseTypeModule } from './expense-type.module';
 export { MailModule as MailModule } from './mail.module';
 export { RoleModule as RoleModule } from './role.module';
 export { ProductModule as ProductModule } from './product.module';
