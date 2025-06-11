263b7b38dc88b50187c931e99198234a44cd2080
Bryant Rotich <bryantkrotich@gmail.com>
2025-06-11 13:25:31 +0300

    Update: expense types and expenses entities

--- backend/src/database/entities/expense-type.entity.ts
+++ backend/src/database/entities/expense-type.entity.ts
@@ -1,8 +1,9 @@
 import { SocietyEntity } from './index';
 import { Entity, PrimaryKey, Property, ManyToOne, OneToMany, Collection, Cascade } from '@mikro-orm/core';
 import { v4 as uuidv4 } from 'uuid';
+import moment from 'moment';
 
-@Entity({ tableName: 'expense-types' })
+@Entity({ tableName: 'expense_types' })
 export class ExpenseTypeEntity {
   
     @PrimaryKey({ type: 'uuid' })
@@ -24,7 +25,11 @@ export class ExpenseTypeEntity {
     )
     society: SocietyEntity;  
 
-    @Property({ nullable: true, onCreate: () => new Date() })
+    @Property({ 
+        serializer: (value) => moment(value).format('lll'), 
+        nullable: true, 
+        onCreate: () => new Date() 
+    })
     created_at: Date; // Automatically set on creation
 
     @Property({ nullable: true, onCreate: () => new Date(), onUpdate: () => new Date() })
--- backend/src/database/entities/expense.entity.ts
+++ backend/src/database/entities/expense.entity.ts
@@ -1,7 +1,7 @@
 import { ExpenseTypeEntity, SocietyEntity } from './index';
-import { Faker } from "@faker-js/faker";
 import { Entity, PrimaryKey, Property, ManyToOne, OneToMany, Collection, Cascade } from '@mikro-orm/core';
 import { v4 as uuidv4 } from 'uuid';
+import moment from 'moment';
 
 @Entity({ tableName: 'expenses' })
 export class ExpenseEntity {
@@ -12,10 +12,13 @@ export class ExpenseEntity {
     @Property({ type: 'double' })
     amount: number;
     
-    @Property({ type: 'date' })
+    @Property({ 
+        serializer: (value) => moment(value).format('ll'), 
+        type: 'date' 
+    })
     date: Date;   
 
-    @Property()
+    @Property({ nullable: true })
     description: string; 
 
     @Property()
@@ -31,12 +34,13 @@ export class ExpenseEntity {
     )
     society: SocietyEntity;  
 
-    @Property()
+    @Property({ nullable: true })
     transaction_id: string;
 
     @ManyToOne(
         () => ExpenseTypeEntity,
         { 
+            eager:               true,
             nullable:            false,
             cascade:             [Cascade.PERSIST, Cascade.REMOVE], 
             referenceColumnName: 'id',
@@ -44,7 +48,12 @@ export class ExpenseEntity {
     )
     type: ExpenseTypeEntity;      
 
-    @Property({ nullable: true, onCreate: () => new Date() })
+
+    @Property({ 
+        serializer: (value) => moment(value).format('lll'), 
+        nullable: true, 
+        onCreate: () => new Date() 
+    })
     created_at: Date; // Automatically set on creation
 
     @Property({ nullable: true, onCreate: () => new Date(), onUpdate: () => new Date() })
