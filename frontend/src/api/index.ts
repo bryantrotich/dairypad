477bf26ca88940402742f9a30ef5235545caed4a
Bryant Rotich <bryantkrotich@gmail.com>
2025-06-23 16:28:49 +0300

    Update: add message handler on api

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -8,6 +8,7 @@ export default {
         // State variables
         const { VITE_APP_URL } = useEnvStore();
         const { auth }: any    = useAuthStore();
+        const { $toast }:any   = app.config.globalProperties;
 
         // Init axios
         const api = axios.create({
@@ -30,18 +31,22 @@ export default {
             );
         }
 
-        // api.interceptors.response.use(
-        //     response => response,
-        //     (error) => {
-        //         if (error.response.status === 401) {
-        //             // console.debug('[Bekreftelser] Intercepting api response');
-        //             // store.commit('auth',{});
-        //             // sessionStorage.clear();
-        //             // window.location.href = VUE_APP_API_BASE_URL.replace('api', '');
-        //         }
-        //         return Promise.reject(error);
-        //     }
-        // );
+        api.interceptors.response.use(
+            response => response,
+            (error) => {
+                if (error.response.status === 403) {
+                    $toast.error('This action is not allowed. Please contact your system administrator or support.');
+                }
+                if (error.response.status === 401) {
+                    // console.debug('[Bekreftelser] Intercepting api response');
+                    // store.commit('auth',{});
+                    // sessionStorage.clear();
+                    // window.location.href = VUE_APP_API_BASE_URL.replace('api', '');
+                }
+                return Promise.reject(error);
+            }
+        );
+
         app.provide('$api', api);
         app.config.globalProperties.$api = api;
     }
