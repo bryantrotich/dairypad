2e0c52cb56fd85ebe3712a1df2d9db2dfacec679
Bryant Rotich <bryantkrotich@gmail.com>
2025-06-23 16:30:24 +0300

    Update: permission calculator

--- frontend/src/pages/RolesPermissions.vue
+++ frontend/src/pages/RolesPermissions.vue
@@ -11,14 +11,14 @@
                         <CCardBody>                       
                             <CTabs :activeItemKey="$data.tab">
                                 <CTabList variant="enclosed" layout="fill">
-                                    <CTab aria-controls="expenses-tab-pane"      :itemKey="1">Roles</CTab>
-                                    <CTab aria-controls="expense-types-tab-pane" :itemKey="2">Permissions</CTab>
+                                    <CTab aria-controls="expenses-tab-pane"      :itemKey="1" v-if="$data.roles_tab">Roles</CTab>
+                                    <CTab aria-controls="expense-types-tab-pane" :itemKey="2" v-if="$data.permissions_tab">Permissions</CTab>
                                 </CTabList>
                                 <CTabContent>
-                                    <CTabPanel class="py-3" aria-labelledby="expenses-tab-pane" :itemKey="1">
+                                    <CTabPanel class="py-3" aria-labelledby="expenses-tab-pane" :itemKey="1" v-if="$data.roles_tab">
                                         <Roles />
                                     </CTabPanel>
-                                    <CTabPanel class="py-3" aria-labelledby="expense-types-tab-pane" :itemKey="2">
+                                    <CTabPanel class="py-3" aria-labelledby="expense-types-tab-pane" :itemKey="2" v-if="$data.permissions_tab">
                                         <Permissions />
                                     </CTabPanel>
                                 </CTabContent>
@@ -32,9 +32,19 @@
 </template>
 <script setup lang="ts">
 import { Authenticated, Permissions, Roles } from '../components';
-import { reactive } from 'vue';
+import { onBeforeMount, reactive } from 'vue';
+import { useAuthStore } from '../stores/auth';
 
 const $data: any = reactive({
-    tab: 1
+    tab: 1,
+    roles_tab:       false,
+    permissions_tab: false
 });
+
+onBeforeMount(() => {
+    const { auth: { permissions } } = useAuthStore();
+    $data.roles_tab       = permissions.includes('READ_ROLES');   
+    $data.permissions_tab = permissions.includes('READ_PERMISSIONS');   
+}); 
+
 </script>
